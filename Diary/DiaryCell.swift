3c95183cbb468a864d69561e5314613df329f467
Minji Kim <minji_0801@naver.com>
2021-10-22 17:28:46 +0900

    기본 UI 구성

---
+++ Diary/DiaryCell.swift
@@ -0,0 +1,12 @@
+//
+//  DiaryCell.swift
+//  Diary
+//
+//  Created by 김민지 on 2021/10/20.
+//
+
+import UIKit
+
+class DiaryCell: UICollectionViewCell {
+    
+}
---
+++ Diary/StarCell.swift
@@ -0,0 +1,12 @@
+//
+//  StarCell.swift
+//  Diary
+//
+//  Created by 김민지 on 2021/10/20.
+//
+
+import UIKit
+
+class StarCell: UICollectionViewCell {
+    
+}
