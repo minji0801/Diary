c112c1a164ca2d9db30bf69a33d57733439cb664
Minji Kim <minji_0801@naver.com>
2021-10-23 21:35:38 +0900

    일기 작성 기능 구현

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 김민지 on 2021/10/23.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStart: Bool
+}
